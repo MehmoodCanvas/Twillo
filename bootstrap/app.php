66ce2bcc711d44372af8e061154b1a1e1e662b6c
Dev <127541405+MehmoodCanvas@users.noreply.github.com>
2024-09-11 01:36:33 +0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'https://wackywinsplatform.com/sms-response',
+            'sms-response',
            
         ]);
     })
