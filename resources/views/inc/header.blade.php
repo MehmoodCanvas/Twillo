bb90886cd442dcfa61e755e4acc8eab737709226
Dev <127541405+MehmoodCanvas@users.noreply.github.com>
2024-11-05 03:39:51 +0500

    s

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -13,6 +13,69 @@
     <link rel="stylesheet" href="{{asset('assets/front/css/main.css')}}">
     <link rel="stylesheet" href="{{asset('assets/front/css/responsive.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style>
+            .legal_top_wrapper {
+            padding: 150px 0px 100px 0px;
+            background-color: var(--text-color);
+        }
+        .legal_wrap_top_text{
+            text-align: center;
+        }
+        .legal_wrap_top_text h6 {
+            font-size: 65px;
+            font-family: var(--Montserrat-SemiBold);
+            color: var(--white);
+            margin: 0 0 20px 0;
+            line-height: 1;
+            text-transform: capitalize;
+        }
+        .legal_wrap_top_text h5 {
+            font-size: 24px;
+            font-family: var(--Montserrat-Medium);
+            color: var(--white);
+            text-decoration: underline;
+            text-transform: uppercase;
+        }
+        .legal_wrapper{
+            padding: 100px 0px;
+        }
+        .legal_wrap_text h6{
+            font-size: 40px;
+            font-family: var(--Montserrat-SemiBold);
+            color: var(--black);
+            margin: 0 0 30px 0;
+            line-height: 1;
+            text-transform: capitalize;
+        }
+        .legal_wrap_text h5{
+            font-size: 30px;
+            font-family: var(--Montserrat-SemiBold);
+            color: var(--black);
+            margin: 0 0 20px 0;
+            line-height: 1;
+            text-transform: capitalize;
+        }
+         .legal_wrap_text p {
+            font-size: 17px;
+            font-family: var(--Montserrat-Medium);
+            color: var(--black);
+            margin: 0 0 20px 0;
+        }
+        .legal_wrap_text ul {
+            padding: 0px 0px 0px 40px;
+            margin: 0px 0px 25px 0px;
+        }
+         .legal_wrap_text ul li {
+            font-size: 17px;
+            font-family: var(--Montserrat-Medium);
+            color: var(--black);
+            list-style: disc;
+            margin: 0 0 10px 0;
+        }
+         .legal_wrap_text ul li span{
+            font-family: var(--Montserrat-SemiBold);
+         }
+</style>
 </head>
 <body>
 
